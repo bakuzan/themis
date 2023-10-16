cfafc61ba50e71af7a9898f6cdb33869a13efb94
bakuzan <imannoyed4days@hotmail.co.uk>
2023-10-16 09:58:29 +0100

    Working history counts table, its even responsive!

--- src/components/MonthCountsTable/index.tsx
+++ src/components/MonthCountsTable/index.tsx
@@ -5,6 +5,7 @@ import { MonthIssueCountViewModel } from '@/types/Stats';
 import { monthNumberToNames } from '@/constants/date';
 
 import styles from './index.module.css';
+import classNames from '@/utils/classNames';
 
 const monthNames = Array.from(monthNumberToNames.values());
 
@@ -12,6 +13,10 @@ interface MonthCountsTableProps {
   data: [number, MonthIssueCountViewModel[]][];
 }
 
+/* TODO
+ * 1) Add a button to the cells, like in Erza, to display a list of the comics that were completed in that month
+ * 2) Expand (1) to include the year cell too
+ */
 export default function MonthCountsTable(props: MonthCountsTableProps) {
   const [today] = useState(new Date());
   const currentYear = today.getFullYear();
@@ -34,40 +39,73 @@ export default function MonthCountsTable(props: MonthCountsTableProps) {
         </tr>
       </thead>
       <tbody>
-        {props.data.map(([year, months]) => (
-          <tr>
-            <th className={styles.tableHeader}>{year}</th>
-            {months.map((m) => {
-              const monthLabelId = `info_${m.monthKey}`;
-              const monthNames = monthNumberToNames.get(m.monthNumber);
-              const monthIndex = m.monthNumber - 1;
-              const isFuture =
-                currentYear > year ||
-                (currentYear === year && currentMonth > monthIndex);
-              const isCurrent =
-                currentYear === year && currentMonth === monthIndex;
+        {props.data.map(([year, months]) => {
+          const yearLabelId = `info_${year}`;
+          const yearCount = months.reduce((p, c) => p + c.count, 0);
 
-              const tense = isCurrent ? 'have been' : 'were';
-              const label = !isFuture
-                ? `In ${monthNames?.long} ${year} ${m.count} comics ${tense} read.`
-                : `${monthNames?.long} ${year} is in the future, therefore no comics have been read.`;
+          const isFuture = currentYear < year;
+          const isCurrent = currentYear === year;
+          const tense = isCurrent ? 'have been' : 'were';
+          const yearLabel = !isFuture
+            ? `In ${year}, ${yearCount} comics ${tense} read.`
+            : `${year} is in the future, therefore no comics have been read yet.`;
 
-              return (
-                <td
-                  key={m.monthKey}
-                  title={label} // TODO make the title a tooltip instead
-                  aria-labelledby={monthLabelId}
-                  className={styles.tableCell}
-                  style={{ opacity: m.count / maxMonthCount }}
-                >
-                  <p id={monthLabelId} className="sr-only">
-                    {label}
-                  </p>
-                </td>
-              );
-            })}
-          </tr>
-        ))}
+          return (
+            <tr key={year}>
+              <th
+                data-tooltip={yearLabel}
+                aria-labelledby={yearLabelId}
+                className={classNames(styles.tableHeader, 'tooltip-right')}
+              >
+                <p id={yearLabelId} className="sr-only">
+                  {yearLabel}
+                </p>
+                {year}
+              </th>
+              {months.map((m, i) => {
+                const isLast = i === 11; // Last month index
+                const monthLabelId = `info_${m.monthKey}`;
+                const monthNames = monthNumberToNames.get(m.monthNumber);
+                const monthIndex = m.monthNumber - 1;
+                const isFuture =
+                  currentYear < year ||
+                  (currentYear === year && currentMonth < monthIndex);
+                const isCurrent =
+                  currentYear === year && currentMonth === monthIndex;
+
+                const tense = isCurrent ? 'have been' : 'were';
+                const label = !isFuture
+                  ? `In ${monthNames?.long} ${year}, ${m.count} comics ${tense} read.`
+                  : `${monthNames?.long} ${year} is in the future, therefore no comics have been read yet.`;
+
+                return (
+                  <td
+                    key={m.monthKey}
+                    data-column-title={monthNames?.short}
+                    aria-labelledby={monthLabelId}
+                    className={classNames(styles.tableCell)}
+                  >
+                    <p id={monthLabelId} className="sr-only">
+                      {label}
+                    </p>
+                    <div
+                      className={classNames(
+                        styles.tableCellInner,
+                        isLast && 'tooltip-left'
+                      )}
+                      data-tooltip={label}
+                    >
+                      <div
+                        className={styles.tableCellContents}
+                        style={{ opacity: m.count / maxMonthCount }}
+                      ></div>
+                    </div>
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
