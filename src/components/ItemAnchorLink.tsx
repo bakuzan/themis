7dadc5b7d7df9790fe39c61b60776b078011e7ea
bakuzan <imannoyed4days@hotmail.co.uk>
2023-12-05 11:56:42 +0000

    Add title/aria to item anchor link

--- src/components/ItemAnchorLink.tsx
+++ src/components/ItemAnchorLink.tsx
@@ -9,7 +9,13 @@ interface ItemAnchorLinkProps {
 export default function ItemAnchorLink({ hashId }: ItemAnchorLinkProps) {
   return (
     <div className={styles.itemAnchor}>
-      <Link href={`#${hashId}`}>#</Link>
+      <Link
+        href={`#${hashId}`}
+        aria-label="Update url to item permalink"
+        title="Permalink"
+      >
+        <span aria-hidden={true}>#</span>
+      </Link>
     </div>
   );
 }
